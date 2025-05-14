b825c8ffc1822cb62f1379b2114262d500c9f5e3
Junbo Huang <junbo.huang@uni-hamburg.de>
2025-05-14 21:27:24 +0200

    :sparkles: change setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='krippendorrf-graph',
+    name='graph-krippendorrf',
     version='0.1.0',
     description='A Python package for computing krippendorrfs alpha for graph (modified from https://github.com/grrrr/krippendorff-alpha/blob/master/krippendorff_alpha.py)',
     url='https://github.com/grrrr/krippendorff-alpha/blob/master/krippendorff_alpha.py',
